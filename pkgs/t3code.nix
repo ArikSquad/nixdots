d089c16f757ef30d4ffffa72874606ff0968a68f
ArikSquad <75741608+ArikSquad@users.noreply.github.com>
2026-08-11 06:13:09 +0000

    chore: update T3 Code

--- pkgs/t3code.nix
+++ pkgs/t3code.nix
@@ -8,11 +8,11 @@
 
 let
   pname = "t3code";
-  version = "0.0.34-nightly.20260810.1059";
+  version = "0.0.34-nightly.20260811.1064";
 
   src = fetchurl {
     url = "https://github.com/pingdotgg/t3code/releases/download/v${version}/T3-Code-${version}-x86_64.AppImage";
-    hash = "sha256-v+uTvVCf93xUfV3lFb5Uz+4wN5Cw0jQ8nQmVGIjvR1g=";
+    hash = "sha256-pHHzks+fjIaMtTgyP2Y1x9+dZ3QzWOtmvbXCXDa9Res=";
   };
 
   app = appimageTools.wrapType2 {
